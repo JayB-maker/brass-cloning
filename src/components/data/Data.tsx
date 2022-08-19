691d804341eec3176a413c8ffb184fdeb8d7c9f4
JayB <ajiboyeabiodun001@gmail.com>
2022-08-19 16:26:01 +0200

    first commit

--- src/components/data/Data.tsx
+++ src/components/data/Data.tsx
@@ -1,1 +1,50 @@
-export {}
+export const navbarDetails = [
+  {
+    menu: "Industry",
+    subMenu: [
+      {
+        heading: "Retail Businessess",
+        details: "Stores, restaurant, offline merchants",
+        path: "/",
+      },
+      {
+        heading: "Internet Companies",
+        details: "Startups, Onlines, API driven companies",
+        path: "/",
+      },
+    ],
+  },
+  {
+    menu: "Customers",
+    path: "/",
+  },
+  {
+    menu: "Capital",
+    path: "/",
+  },
+  {
+    menu: "Company",
+    subMenu: [
+      {
+        heading: "About us",
+        details: "Learn more about our mission",
+        path: "/",
+      },
+      {
+        heading: "Why Brass",
+        details: "Our story and our path forward",
+        path: "/",
+      },
+      {
+        heading: "Careers",
+        details: "Explore opportunities with us",
+        path: "/",
+      },
+      {
+        heading: "Press resources",
+        details: "Press assets and guidelines",
+        path: "/",
+      },
+    ],
+  },
+];
