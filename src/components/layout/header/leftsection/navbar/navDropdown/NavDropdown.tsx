a1a3e79d8658854750078754ee8260b99b549b6f
JayB <ajiboyeabiodun001@gmail.com>
2022-08-19 16:26:28 +0200

    first commit

---
+++ src/components/layout/header/leftsection/navbar/navDropdown/NavDropdown.tsx
@@ -0,0 +1,33 @@
+import {
+  Card,
+  LinkText,
+  primaryColor,
+  Text,
+  TitleText,
+} from "../../../../../ui";
+import { navProps } from "../Navbar";
+
+const NavDropdown = (props: navProps) => {
+  const { path, heading, details } = props;
+  return (
+    <Card>
+      <LinkText
+        flex
+        padding="15px 20px"
+        hbg="#EBEDEE"
+        flexdirection="column"
+        width="100%"
+        to={path}
+      >
+        <TitleText size="14px" lineheight="19px" color={primaryColor}>
+          {heading}
+        </TitleText>
+        <Text size="14px" lineheight="19px" color={primaryColor}>
+          {details}
+        </Text>
+      </LinkText>
+    </Card>
+  );
+};
+
+export default NavDropdown;
