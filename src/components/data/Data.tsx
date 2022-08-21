958ad7996ee33df92eca239c32a2a8bf0533f026
JayB <ajiboyeabiodun001@gmail.com>
2022-08-22 00:39:24 +0200

    new commit

--- src/components/data/Data.tsx
+++ src/components/data/Data.tsx
@@ -48,3 +48,42 @@ export const navbarDetails = [
     ],
   },
 ];
+
+export const resourcesDetails = [
+  {
+    id: "10",
+    menu: "Resources",
+    subMenu: [
+      {
+        heading: "Blog",
+        details: "Stay updated on news and products",
+        path: "/",
+      },
+      {
+        heading: "Guides",
+        details: "Everything your business needs to know",
+        path: "/",
+      },
+      {
+        heading: "Help",
+        details: "Get support for your business",
+        path: "/",
+      },
+    ],
+  },
+];
+
+
+export const TrusteesDetails = [{
+  flex: "3",
+  image: "assets/techchrunch.png",
+  path: "https://techcrunch.com/2021/10/21/ventures-platform-hustle-fund-back-nigerian-fintech-brass-in-1-7m-round/",
+}, {
+  flex: "2",
+  image: "assets/yahoo.png",
+  path: "https://www.yahoo.com/now/ventures-platform-hustle-fund-back-080006151.html?guccounter=1",
+}, {
+  flex: "2",
+  image: "assets/business-insider.png",
+  path: "https://africa.businessinsider.com/local/markets/nigerian-fintech-brass-secures-dollar17mn-financing-round-to-power-expansion-into/74mwmjq",
+}, ]
