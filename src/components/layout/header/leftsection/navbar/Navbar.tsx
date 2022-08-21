2ef555157ca42852649f7a54c4ddfc8779040c65
JayB <ajiboyeabiodun001@gmail.com>
2022-08-22 00:40:17 +0200

    new commit

--- src/components/layout/header/leftsection/navbar/Navbar.tsx
+++ src/components/layout/header/leftsection/navbar/Navbar.tsx
@@ -8,43 +8,53 @@ export interface navProps {
   heading?: string;
   subMenu?: Array<object>;
   path?: any;
+  id?:any;
   handle?: string;
   details?: string;
   menu?: string;
 }
 
 const Navbar = (props: navProps) => {
-  const [hover, setHover] = useState(false);
+  const [hover, setHover] = useState(null);
+
+  const toggle = (index:any) => {
+    if (hover === index) {
+      return setHover(null);
+    }
+
+    setHover(index);
+  };
+
   return (
     <>
-      <Card flex gap="32px" alignitems="center">
+      <Card flex gap="32px" alignitems="center" mddisplay="none">
         {navbarDetails.map((navitem, index) => (
           <Card key={index}>
             {navitem.subMenu ? (
               <>
                 <Card
                   position="relative"
-                  onMouseEnter={() => setHover(true)}
-                  onMouseLeave={() => setHover(false)}
+                  onMouseEnter={() => toggle(index)}
+                  onMouseLeave={() => toggle(index)}
                 >
                   <TitleText
                     pointer
                     margin="2px 0 0"
+                    weight="600"
                     size="14px"
-                    lineheight="20px"
+                    lineheight="23px"
                     color={whiteColor}
                   >
                     {navitem.menu}
                   </TitleText>
-                  {hover && (
+                  {hover === index && (
                     <Card position="absolute" top="20px" left="0">
                       <Card
                         radius="5px"
                         padding="10px 0"
                         flex
                         flexdirection="column"
-                        margin="20px 0 0"
-                        gap="10px"
+                        margin="25px 0 0"
                         width="320px"
                         bg={whiteColor}
                         shadow="0 3px 3px rgba(0, 0, 0, 0.1)"
@@ -68,7 +78,8 @@ const Navbar = (props: navProps) => {
                 <Card>
                   <LinkText
                     size="14px"
-                    lineheight="20px"
+                    lineheight="23px"
+                    weight="600"
                     color={whiteColor}
                     to={navitem.path}
                   >
