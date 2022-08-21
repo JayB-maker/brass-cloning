b45b73c4109698a7d2146b520ef93931f8d14830
JayB <ajiboyeabiodun001@gmail.com>
2022-08-22 00:39:17 +0200

    new commit

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -9,9 +9,4 @@ export const GlobalStyle = createGlobalStyle`
     text-decoration: none;
     list-style: none;
 }
-
-Body{
-    // background: black;
-    font-family: 'Lexend Deca', sans-serif;
-}
 `;
