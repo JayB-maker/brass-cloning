ccc26973dc29329ad9fe5dba6c501e85b3925fd2
JayB <ajiboyeabiodun001@gmail.com>
2022-08-22 00:40:07 +0200

    new commit

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,10 +1,32 @@
-import { Card, InnerSection, MainSection, primaryColor } from "../../ui";
+import { useState } from "react";
+import styled from "styled-components";
+import { mobileMenuContext } from "../../../helper/Context";
+import {
+  accentColor,
+  Card,
+  InnerSection,
+  LinkText,
+  MainSection,
+  primaryColor,
+  whiteColor,
+} from "../../ui";
 import LeftSection from "./leftsection/LeftSection";
+import MobileMenu from "./mobileMenu/MobileMenu";
 import RightSection from "./rightSection/RightSection";
 
+const Icon = styled.i`
+  display: none;
+
+  @media screen and (max-width: 1024px) {
+    display: block;
+  }
+`;
+
 const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
   return (
     <>
+    <mobileMenuContext.Provider value={{mobileMenu, setMobileMenu}}>
       <MainSection
         width="100%"
         bg={primaryColor}
@@ -21,12 +43,49 @@ const Header = () => {
             width="100%"
             height="80px"
             alignitems="center"
+            color={whiteColor}
           >
             <LeftSection />
             <RightSection />
+            <Icon
+              className={mobileMenu ? "fa-solid fa-times" : "fa-solid fa-bars"}
+              onClick={() => setMobileMenu(!mobileMenu)}
+            />
           </Card>
         </InnerSection>
       </MainSection>
+      {mobileMenu && (
+        <Card
+          transition="2000ms"
+          position="fixed"
+          left={mobileMenu ? "0" : "100%"}
+          index="6"
+          top="0"
+        >
+          <MobileMenu />
+        </Card>
+      )}
+      <LinkText
+        display="none"
+        smdisplay="block"
+        bg={accentColor}
+        mdpadding="15px 0"
+        smpadding="25px 0"
+        align="center"
+        width="100vw"
+        position="fixed"
+        bottom="0"
+        left="0"
+        index="100"
+        color={whiteColor}
+        size="15px"
+        lineheight="23px"
+        weight="700"
+        to="/"
+      >
+        Open an account in 10 minutes
+      </LinkText>
+      </mobileMenuContext.Provider>
     </>
   );
 };
