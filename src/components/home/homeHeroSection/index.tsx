107a4d1c12ce929c66e68fdb79cab12e9b0fb184
JayB <ajiboyeabiodun001@gmail.com>
2022-08-23 12:18:07 +0200

    new commit

--- src/components/home/homeHeroSection/index.tsx
+++ src/components/home/homeHeroSection/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Card, InnerSection, MainSection, primaryColor } from "../../ui";
+import LeftSection from "./leftSection/LeftSection";
+import RightSection from "./rightSection/RightSection";
 
 const HomeHeroSection = () => {
   return (
-    <div>HomeHeroSection</div>
-  )
-}
+    <>
+      <MainSection width="100%" bg={primaryColor}>
+        <InnerSection>
+          <Card
+            flex
+            alignitems="center"
+            height="100vh"
+            mdheight="unset"
+            padding="60px 0 60px"
+            mdpadding="20px 0 60px"
+            gap="50px"
+            smgap="40px"
+            xmdflexdirection="column-reverse"
+            xmdheight="unset"
+          >
+            <LeftSection />
+            <RightSection />
+          </Card>
+        </InnerSection>
+      </MainSection>
+    </>
+  );
+};
 
-export default HomeHeroSection
+export default HomeHeroSection;
