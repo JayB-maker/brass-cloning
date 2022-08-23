d18c46987ad088ce30252d46963224bc6ff3e507
JayB <ajiboyeabiodun001@gmail.com>
2022-08-23 12:20:48 +0200

    new commit

--- src/components/layout/header/rightSection/RightSection.tsx
+++ src/components/layout/header/rightSection/RightSection.tsx
@@ -1,9 +1,43 @@
-import { Card } from "../../../ui"
+import {
+  accentColor,
+  Card,
+  LinkText,
+  TitleText,
+  whiteColor,
+} from "../../../ui";
+import Resources from "./Resources";
 
 const RightSection = () => {
   return (
-    <Card color="white">Right Section</Card>
-  )
-}
+    <>
+      <Card flex gap="32px" alignitems="center" mddisplay="none">
+        <Resources />
+        <LinkText
+          margin="2px 0 0"
+          size="14px"
+          lineheight="23px"
+          weight="600"
+          color={whiteColor}
+          to="/"
+        >
+          Login
+        </LinkText>
+        <LinkText
+          margin="2px 0 0"
+          padding="11px 32px"
+          weight="600"
+          bg={accentColor}
+          radius="5px"
+          size="14px"
+          lineheight="23px"
+          color={whiteColor}
+          to="/"
+        >
+          Open an account
+        </LinkText>
+      </Card>
+    </>
+  );
+};
 
-export default RightSection
+export default RightSection;
