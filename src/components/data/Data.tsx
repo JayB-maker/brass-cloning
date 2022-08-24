7f0302ccff8c114e4f18d2b0dc5898d641bf2bdb
JayB <ajiboyeabiodun001@gmail.com>
2022-08-24 11:20:42 +0200

    new commit

--- src/components/data/Data.tsx
+++ src/components/data/Data.tsx
@@ -73,31 +73,69 @@ export const resourcesDetails = [
   },
 ];
 
+export const TrusteesDetails = [
+  {
+    flex: "3",
+    image: "assets/techchrunch.png",
+    path: "https://techcrunch.com/2021/10/21/ventures-platform-hustle-fund-back-nigerian-fintech-brass-in-1-7m-round/",
+  },
+  {
+    flex: "2",
+    image: "assets/yahoo.png",
+    path: "https://www.yahoo.com/now/ventures-platform-hustle-fund-back-080006151.html?guccounter=1",
+  },
+  {
+    flex: "2",
+    image: "assets/business-insider.png",
+    path: "https://africa.businessinsider.com/local/markets/nigerian-fintech-brass-secures-dollar17mn-financing-round-to-power-expansion-into/74mwmjq",
+  },
+];
 
-export const TrusteesDetails = [{
-  flex: "3",
-  image: "assets/techchrunch.png",
-  path: "https://techcrunch.com/2021/10/21/ventures-platform-hustle-fund-back-nigerian-fintech-brass-in-1-7m-round/",
-}, {
-  flex: "2",
-  image: "assets/yahoo.png",
-  path: "https://www.yahoo.com/now/ventures-platform-hustle-fund-back-080006151.html?guccounter=1",
-}, {
-  flex: "2",
-  image: "assets/business-insider.png",
-  path: "https://africa.businessinsider.com/local/markets/nigerian-fintech-brass-secures-dollar17mn-financing-round-to-power-expansion-into/74mwmjq",
-}, ]
+export const FinancialStacksDetails = [
+  {
+    icon: "assets/speed.gif",
+    title: "Speed",
+    details:
+      "Open a commercial-grade current account from anywhere in 10 minutes",
+  },
+  {
+    icon: "assets/security.gif",
+    title: "Security",
+    details:
+      "Rely on bank-grade security with all your monies fully insured by NDIC",
+  },
+  {
+    icon: "assets/wave.gif",
+    title: "Support",
+    details:
+      "Get access to a trusted support team and resources whenever you want",
+  },
+];
 
-export const FinancialStacksDetails = [{
-  icon: "assets/speed.gif",
-  title: "Speed",
-  details: "Open a commercial-grade current account from anywhere in 10 minutes",
-}, {
-  icon: "assets/security.gif",
-  title: "Security",
-  details: "Rely on bank-grade security with all your monies fully insured by NDIC",
-}, {
-  icon: "assets/wave.gif",
-  title: "Support",
-  details: "Get access to a trusted support team and resources whenever you want",
-}, ]
+export const EverythingYouNeedDetais = [
+  {
+    title: "Payments",
+    details: "Create instant, scheduled and recurring payments easily",
+    video: "assets/brass-vid-1.mp4",
+  },
+  {
+    title: "Invoice",
+    details: "Send, receive, and reconcile invoices all on one platform",
+    video: "assets/brass-vid-2.mp4",
+  },
+  {
+    title: "Cashflow Support",
+    details: "Move your business forward with insight and financial resources",
+    video: "assets/brass-vid-3.mp4",
+  },
+  {
+    title: "Cross-platform availability",
+    details: "Bank everywhere. Available on iOS, Android and Slack",
+    video: "assets/brass-vid-4.mp4",
+  },
+  {
+    title: "Team Banking",
+    details: "Work with your team to run your entire financial operations",
+    video: "assets/brass-vid-5.mp4",
+  },
+];
