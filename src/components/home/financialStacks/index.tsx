94b6dfc50cffe69b2864d504350651d1ac06f515
JayB <ajiboyeabiodun001@gmail.com>
2022-08-24 06:56:52 +0200

    new commit

--- src/components/home/financialStacks/index.tsx
+++ src/components/home/financialStacks/index.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Card, InnerSection, MainSection, Text, TitleText } from "../../ui";
+import FinancialStacksCard from "./financialStacksCard/FinancialStacksCard";
 
 const FinancialStacks = () => {
   return (
-    <div>FinancialStacks</div>
-  )
-}
+    <MainSection padding="98px 0 0">
+      <InnerSection>
+        <Card>
+          <TitleText textalign="center" size="30px" lineheight="41px" heavy>
+            The complete financial stack for your business
+          </TitleText>
+          <Text
+            center
+            size="18px"
+            mdsize="15px"
+            lineheight="26px"
+            margin="16px 0 0"
+          >
+            Top-end financial tools to help your business with its financial
+            operations and cash flow.
+          </Text>
+          <Text center size="18px" mdsize="15px" lineheight="26px">
+            {" "}
+            We help companies of all types work better, save time and money with
+            a simply better banking service that works.
+          </Text>
+        </Card>
+        <FinancialStacksCard />
+      </InnerSection>
+    </MainSection>
+  );
+};
 
-export default FinancialStacks
+export default FinancialStacks;
