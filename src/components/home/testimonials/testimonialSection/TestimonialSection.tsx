365a63462b8001a525d27cd92f6781b510c581a2
JayB <ajiboyeabiodun001@gmail.com>
2022-09-05 23:38:14 +0200

    New commit

---
+++ src/components/home/testimonials/testimonialSection/TestimonialSection.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { TestimonialsContext } from "../../../../helper/Context";
+import { Card } from "../../../ui";
+import LeftSection from "./LeftSection/LeftSection";
+import RightSection from "./rightSection/RightSection";
+
+const TestimonialSection = () => {
+  const [clicked, setClicked] = useState(2);
+  return (
+    <>
+      <TestimonialsContext.Provider value={{ clicked, setClicked }}>
+        <Card flex maxwidth="1440px" margin="50px 0 0" width="100%" bg="black">
+          <LeftSection />
+          <RightSection />
+        </Card>
+      </TestimonialsContext.Provider>
+    </>
+  );
+};
+
+export default TestimonialSection;
