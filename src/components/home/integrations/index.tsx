9c5530c7bf9e6b981df66b68180eea7cb83f0d7e
JayB <ajiboyeabiodun001@gmail.com>
2022-09-06 03:25:42 +0200

    new commit

--- src/components/home/integrations/index.tsx
+++ src/components/home/integrations/index.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Card, InnerSection, MainSection, Text, TitleText } from '../../ui'
+import IntegrationTools from './integrationTools/IntegrationTools'
 
 const Integration = () => {
   return (
-    <div>Integration</div>
+    <MainSection padding="100px 0" mdpadding="50px 0" smpadding="30px 0">
+      <InnerSection>
+        <Card
+          flex
+          flexdirection="column"
+          width="100%"
+          gap="16px"
+        >
+          <TitleText
+            size="45px"
+            heavy
+            lineheight="58px"
+            mdsize="30px"
+            mdlineheight="41px"
+          >
+            Integrations that matter
+          </TitleText>
+          <Text
+            size="18px"
+            lineheight="26px"
+            mdsize="15px"
+            mdlineheight="20px"
+          >
+            Brass is seamlessly integrated with the tools your team is already familiar with.
+          </Text>
+          <IntegrationTools />
+        </Card>
+      </InnerSection>
+    </MainSection>
   )
 }
 
