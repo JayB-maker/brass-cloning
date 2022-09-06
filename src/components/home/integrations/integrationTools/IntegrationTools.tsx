8741d5b96ed9b445cbff29b7c5353f52079b0dd9
JayB <ajiboyeabiodun001@gmail.com>
2022-09-06 03:25:58 +0200

    new commit

---
+++ src/components/home/integrations/integrationTools/IntegrationTools.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { IntegrationDetails } from "../../../data/Data";
+import { Card } from "../../../ui";
+import IntegrationCards from "./IntegrationCards";
+
+const IntegrationTools = () => {
+  return (
+    <>
+      <Card flex gap="24px" overflowx="scroll" width="100%">
+        {IntegrationDetails.map((item, index) => (
+          <Card key={index}>
+            <IntegrationCards
+              icon={item.iconSRC}
+              tool={item.tools}
+              details={item.details}
+              background={item.backgroundColor}
+            />
+          </Card>
+        ))}
+      </Card>
+    </>
+  );
+};
+
+export default IntegrationTools;
