7fc67466255f0b441ed8324073064e89ffc9e5b1
JayB <ajiboyeabiodun001@gmail.com>
2022-09-06 02:09:20 +0200

    New commit

--- src/helper/Context.tsx
+++ src/helper/Context.tsx
@@ -3,6 +3,10 @@ import { createContext } from "react";
 interface ContextState {
     mobileMenu?: boolean;
     setMobileMenu?: any;
+    clicked?: any;
+    setClicked?: any;
 }
 
 export const mobileMenuContext = createContext({} as ContextState);
+export const everythingYouNeedContext = createContext({} as ContextState);
+export const TestimonialsContext = createContext({} as ContextState);
