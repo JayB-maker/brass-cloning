04d384c82f3e07af2df992e9f3bd56e632109a2d
JayB <ajiboyeabiodun001@gmail.com>
2022-09-06 03:25:49 +0200

    new commit

---
+++ src/components/home/integrations/integrationTools/IntegrationCards.tsx
@@ -0,0 +1,33 @@
+import { Card, Image, Text, TitleText } from "../../../ui";
+
+const IntegrationCards = (props: any) => {
+  const { tool, icon, details, background } = props;
+  return (
+    <>
+      <Card
+        width="292px"
+        height="325px"
+        padding="56px 28px"
+        border="1px solid #8F96A3"
+        radius="8px"
+      >
+        <Card
+          width="48px"
+          height="48px"
+          radius="50%"
+          bg={background}
+          flex
+          alignitems="center"
+          justifycontent="center"
+          margin="0 0 32px"
+        >
+          <Image src={icon} alt={tool} width="80%" />
+        </Card>
+        <TitleText size="20px" lineheight="30px" margin="0 0 16px" >{tool}</TitleText>
+        <Text size="15px" lineheight="20px">{details}</Text>
+      </Card>
+    </>
+  );
+};
+
+export default IntegrationCards;
