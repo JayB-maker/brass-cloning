9221faf9e6fd6f624c14da5799b5f1ce8789a3b6
JayB <ajiboyeabiodun001@gmail.com>
2022-09-07 22:01:39 +0200

    new commit

--- src/components/home/support/index.tsx
+++ src/components/home/support/index.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Card, MainSection } from "../../ui";
+import LeftSection from "./leftSection/LeftSection";
+import RightSection from "./rightSection/RightSection";
 
 const Support = () => {
   return (
-    <div>Support</div>
-  )
-}
+    <MainSection
+      padding="100px 0"
+      mdpadding="50px 0"
+      maxwidth="1440px"
+      width="100%"
+      smpadding="30px 0"
+    >
+      <Card flex gap="20px" smgap="50px" alignitems="center" smflexdirection="column">
+        <LeftSection />
+        <RightSection />
+      </Card>
+    </MainSection>
+  );
+};
 
-export default Support
+export default Support;
