b3c9328966c5a9c7aab6694059e1e2d6d932befa
JayB <ajiboyeabiodun001@gmail.com>
2022-09-07 22:02:09 +0200

    new commit

--- src/components/home/getStarted/index.tsx
+++ src/components/home/getStarted/index.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Card, InnerSection, MainSection, Text, TitleText } from "../../ui";
+import GetStartedCard from "./getStartedCard/GetStartedCard";
 
 const GetStarted = () => {
   return (
-    <div>GetStarted</div>
-  )
-}
+    <MainSection padding="100px 0" mdpadding="50px 0" smpadding="30px 0">
+      <InnerSection>
+        <Card
+          flex
+          flexdirection="column"
+          width="100%"
+          alignitems="center"
+          gap="16px"
+          justifycontent="center"
+        >
+          <TitleText
+            width="585px"
+            mdwidth="100%"
+            size="45px"
+            heavy
+            textalign="center"
+            lineheight="58px"
+            mdsize="30px"
+            mdlineheight="41px"
+          >
+            Go further with resources to get started
+          </TitleText>
+          <Text
+            width="595px"
+            mdwidth="75%"
+            smwidth="100%"
+            size="18px"
+            center
+            lineheight="26px"
+            mdsize="15px"
+            mdlineheight="20px"
+          >
+            We have partnered with businesses on the same mission as ours to
+            help local businesses. We have also added resources you need to
+            start, run and grow your business.
+          </Text>
+          <GetStartedCard />
+        </Card>
+      </InnerSection>
+    </MainSection>
+  );
+};
 
-export default GetStarted
+export default GetStarted;
