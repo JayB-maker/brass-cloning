001ad0c08716aeac0a44959e9b16d7bb76e03153
JayB <ajiboyeabiodun001@gmail.com>
2022-09-08 22:19:01 +0200

    new commit

--- src/components/home/faq/index.tsx
+++ src/components/home/faq/index.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Card, InnerSection, MainSection, primaryColor } from "../../ui"
+import LeftSection from "./leftSection/LeftSection"
+import RightSection from "./rightSection/RightSection"
 
 const Faq = () => {
   return (
-    <div>Faq</div>
+    <MainSection bg={primaryColor} padding="200px 0 100px" mdpadding="50px 0" smpadding="30px 0">
+      <InnerSection>
+        <Card
+          flex
+          smflexdirection="column"
+        >
+          <LeftSection />
+          <RightSection />
+        </Card>
+      </InnerSection>
+    </MainSection>
   )
 }
 
