76736ccd59359b81a83308e7464393434a6237fe
JayB <ajiboyeabiodun001@gmail.com>
2022-09-13 12:45:45 +0200

    new commit

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Card, InnerSection, MainSection, primaryColor } from '../../ui'
+import BigStartSmall from './bigStartSmall/BigStartSmall'
+import MainFooter from './mainFooter/MainFooter'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <MainSection padding="50px 0" mdpadding="50px 0" smpadding="30px 0" bg={primaryColor}>
+      <InnerSection>
+        <Card
+          flex
+          flexdirection="column"
+          width="100%"
+          gap="172px"
+        >
+          <MainFooter />
+          <BigStartSmall />
+        </Card>
+      </InnerSection>
+    </MainSection>
   )
 }
 
