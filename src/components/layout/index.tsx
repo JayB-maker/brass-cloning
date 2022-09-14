7abbbf6be1aec1d0ebde13f3d2f24a5e17fb451c
JayB <ajiboyeabiodun001@gmail.com>
2022-09-14 12:45:51 +0200

    new commit

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,15 +2,15 @@ import { Card } from "../ui";
 import Footer from "./footer";
 import Header from "./header";
 
-const Layout = (props:any) => {
-  const {children} = props;
+const Layout = (props: any) => {
+  const { children } = props;
   return (
     <>
-    <Header />
-    <Card margin="80px 0 0">{children}</Card>
-    <Footer />
-  </>
-  )
+      <Header />
+      <Card margin="80px 0 0">{children}</Card>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
