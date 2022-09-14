1681f3ff54ec7738c45dbed0ee63ab60eb60404b
JayB <ajiboyeabiodun001@gmail.com>
2022-09-14 12:31:49 +0200

    new commit

--- src/components/layout/footer/bigStartSmall/BigStartSmall.tsx
+++ src/components/layout/footer/bigStartSmall/BigStartSmall.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import { Card } from '../../../ui'
+import React from "react";
+import { Card, Image, Text, TitleText, whiteColor } from "../../../ui";
+import FooterCTA from "./FooterCTA";
 
 const BigStartSmall = () => {
   return (
-    <Card></Card>
-  )
-}
+    <Card flex flexdirection="column" gap="70px">
+      <Card flex flexdirection="column" gap="16px" color={whiteColor}>
+        <Card flex gap="12px" alignitems="flex-end">
+          <Image
+            width="32px"
+            height="32px"
+            src="assets/brass-white.png"
+            alt="brass-icon"
+          />
+          <TitleText size="16px" color={whiteColor}>
+            Big starts small
+          </TitleText>
+        </Card>
+        <Text size="15px" lineheight="20px">
+          Available on web and also on
+        </Text>
+        <FooterCTA />
+      </Card>
+      <Card width="63%" mdwidth="100%" color={whiteColor}>
+        <Text size="16px" lineheight="24px">
+          Banking services backed by Bancorp MFB & FCMB and regulated by the
+          Central Bank of Nigeria. Brass Technology Nigeria Limited, DBA: Brass
+          is a Brass Technologies, Inc company.
+        </Text>
+      </Card>
+    </Card>
+  );
+};
 
-export default BigStartSmall
+export default BigStartSmall;
